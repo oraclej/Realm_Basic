f7e4a57b4885056f095208ee7f7fa0d63adf68f1
Arash <a.hemmati@gmail.com>
2023-09-28 12:45:09 +0330

    All done

--- src/main/java/ir/oraclej/realm_basic/HelloServlet.java
+++ src/main/java/ir/oraclej/realm_basic/HelloServlet.java
@@ -5,24 +5,13 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>I am User Section</h1>");
         out.println("</body></html>");
-    }
-
-    public void destroy() {
     }
 }
